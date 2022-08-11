d03bb8b66f8f94567112f9f3d037469a35ce8fec
vineela13 <vinnybaddepudi@gmail.com>
2022-08-11 16:39:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+function changeName() {
+    let text = document.getElementById('text').value;
+    document.getElementById('pText').innerHTML = text;
+    let text1 = document.getElementById('text1').value;
+    document.getElementById('pText1').innerText = text1;
+}
